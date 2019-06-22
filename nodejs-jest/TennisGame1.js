ef98a1bc30a875e23ac5e43be6f3151a322ab5d0
Michael Cheng <mcheng.work@gmail.com>
2019-06-22 15:22:01 +0800

    Refactor game logic for readability.

--- nodejs-jest/TennisGame1.js
+++ nodejs-jest/TennisGame1.js
@@ -5,6 +5,7 @@ class TennisGame1 {
         this.player1 = player1
         this.player2 = player2
         this.players = [this.player1, this.player2]
+        this.pointMapping = ['Love', 'Fifteen', 'Thirty', 'Forty']
     }
 
     wonPoint(playerName) {
@@ -18,36 +19,49 @@ class TennisGame1 {
     }
 
     getAdvantage() {
+        let advantagePlayer = null
+        let difference = 0
+
         if (this.player1.points > this.player2.points) {
-            return [this.player1, (this.player1.points - this.player2.points)]
-        } else {
-            return [this.player2, (this.player2.points - this.player1.points)]
+            advantagePlayer = this.player1
+            difference = this.player1.points - this.player2.points
+        } else if (this.player1.points < this.player2.points) {
+            advantagePlayer = this.player2
+            difference = this.player2.points - this.player1.points
         }
+
+        return [advantagePlayer, difference]
     }
 
     getScore() {
-        var score = "";
+        const [advantagePlayer, difference] = this.getAdvantage()
 
-        if (this.player1.points === this.player2.points) {
-            const equalScores = ['Love-All', 'Fifteen-All', 'Thirty-All']
-            if (this.player1.points < 3) {
-                score = equalScores[this.player1.points]
-            } else {
-                score = 'Deuce'
-            }
-        } else if (this.player1.points >= 4 || this.player2.points >= 4) {
-            const [advantagePlayer, difference] = this.getAdvantage()
-
-            if (difference >= 2) {
-                score = `Win for ${advantagePlayer.name}`
-            } else {
-                score = `Advantage ${advantagePlayer.name}`
-            }
-        } else {
-            const pointMapping = ['Love', 'Fifteen', 'Thirty', 'Forty']
-            score = `${pointMapping[this.player1.points]}-${pointMapping[this.player2.points]}`
+        if (this.isGameTied()) {
+            return this.handleEqualScores()
+        } else if (this.hasGamePassedWinningThreshold() && difference >= 2) {
+            return `Win for ${advantagePlayer.name}`
+        } else if (this.hasGamePassedWinningThreshold() && difference < 2) {
+            return `Advantage ${advantagePlayer.name}`
         }
-        return score;
+
+        return `${this.pointMapping[this.player1.points]}-${this.pointMapping[this.player2.points]}`
+    }
+
+    hasGamePassedWinningThreshold() {
+        return Math.max(this.player1.points, this.player2.points) >= 4
+    }
+
+    isGameTied() {
+        return this.player1.points === this.player2.points
+    }
+
+    handleEqualScores() {
+        const equalScores = ['Love-All', 'Fifteen-All', 'Thirty-All']
+        if (this.player1.points < 3) {
+            return equalScores[this.player1.points]
+        } else {
+            return 'Deuce'
+        }
     }
 }
 
